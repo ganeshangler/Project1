3fd5eafd20323fdf23247058ce25abca189e8174
ganeshangler <ganeshangler@gmail.com>
2017-09-25 11:03:05 +0530

    Master Setup updated with Jar files placed in Src folder for easy access

--- src/config/Constants.java
+++ src/config/Constants.java
@@ -3,7 +3,7 @@ package config;
 public class Constants 
 {
     public static final String baseUrl="http://192.168.0.13/showtime_club/admin_live";
-    public static final String CHROME_DRIVER_EXEPATH =System.getProperty("user.dir")+ "\\src\\utility\\chromedriver.exe";
+    public static final String CHROME_DRIVER_EXEPATH =System.getProperty("user.dir")+ "\\src\\jarfiles\\chromedriver.exe";
 	public static final String CHROME_DRIVER ="webdriver.chrome.driver";
 	public static final int implicitWaitSec = 60;
 	public static String getPath_TestData=System.getProperty("user.dir")+"\\src\\testData\\";
--- src/utility/Utilis.java
+++ src/utility/Utilis.java
@@ -2,7 +2,6 @@ package utility;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
